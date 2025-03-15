488899570bc3700c6f10fd68d1c14a5bb6f02e30
Alamin-22 <mdalaminmollick@gmail.com>
2025-03-16 00:14:22 +0600

    completed the chapter 1 basic

---
+++ Problem-Solving/Chapter-1/02_problem.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int r = 6;
+
+    int height = 10;
+
+    printf("The area of the circle  with radius %d is =>>  ( %f )       \n", r, (3.1416 * r * r));
+
+    printf("The volume of the cylinder  with radius %d and height %d  is  =>>  ( %f )    \n", r, height, (3.1416 * r * r));
+
+    return 0;
+}
---
+++ Problem-Solving/Chapter-1/04_problem.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float p = 34.3;
+    int r = 8;
+    int t = 5;
+
+    printf("THe value of simple interest is =>>> ( %f )    \n", ((p * r * t) / 100));
+
+    return 0;
+}
