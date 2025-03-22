69d968438da7d3602b8eed448284e208649ae331
Alamin-22 <mdalaminmollick@gmail.com>
2025-03-23 02:22:43 +0600

    added question to all the soluations

--- BeeCrowd-problem-solving/00_Problem.c
+++ BeeCrowd-problem-solving/00_Problem.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+/*
 
+
+Problem- 1000
+Welcome to beecrowd!
+
+Your first program in any programming language is usually "Hello World!". In this first problem all you have to do is print this message on the screen.
+
+Input
+This problem has no input.
+
+Output
+You must print the message Hello World! and then the endline as shown below.
+
+*/
 int main()
 {
-
-    /**
-     * Escreva a sua solução aqui
-     * Code your solution here
-     * Escriba su solución aquí
-     */
 
     printf("Hello World!\n");
 
